46da0af28d93a0fe833504b0a45e699c067343e3
Pedro Silva <tagido@gmail.com>
2017-03-07 17:32:32 +0000

    Code beautifying

--- audio/scripts/hdpureaudio2flac.rb
+++ audio/scripts/hdpureaudio2flac.rb
@@ -1,15 +1,31 @@
 #
-# hdpureaudio2flac.rb
+#   hdpureaudio2flac.rb
+#   ===================
+#   Converts a Bluray HD Pure Audio backup to FLAC lossless audio format
 #
-#
+#   Copyright (C) 2016 Pedro Mendes da Silva 
+# 
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+# 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+# 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 
 
 def convert_chapter start_time,end_time,file_index
-	artist="Amy Winehouse"
-	album= "Back to Black"
+	artist="DJ Estaline"
+	album= "Martelos"
 	date=  "2006"
-	genre= "R&B"
-
+	genre= "Hip-Hop"
 
    metadata = "-metadata title=\"Track #{file_index}\" -metadata artist=\"#{artist}\" -metadata genre=\"#{genre}\" -metadata date=\"#{date}\" -metadata album=\"#{album}\" -metadata track=\"#{file_index}\""
    conv_command = "\"#{FFMPEG_PATH}ffmpeg.exe\" -i bluray:#{BLURAY_PATH} -map 0:1 #{metadata} -ss #{start_time} -to #{end_time} \"#{TARGET_PATH}#{artist} - #{album} - Track #{file_index}.flac\""
@@ -27,24 +43,20 @@ TARGET_FILENAME="Bluray - Track "
 
 GET_CHAPTERS_COMMAND=EAC3TO_PATH+"eac3to.exe . 1) 1: cap.txt"
 
-#system EAC3TO_PATH+"eac3to.exe . 1) 1: cap.txt"
-
+# TODO: automate dependencies and directories (currently hardcoded)
 puts "hdpureaudio2flac.rb - Converts a Bluray HD Pure Audio backup to FLAC"
 puts "-------------\n\n"
 puts "Reading Bluray Disk structure ...\n\n"
 
-#xpto = `\"#{EAC3TO_PATH}eac3to.exe" " #{BLURAY_PATH} 1) 1: cap.txt\"`
+
 system "\"#{EAC3TO_PATH}eac3to.exe\" #{BLURAY_PATH} 1) 1: cap.txt\""
 #print "#### xpto=",xpto," \n"
 
 stats_raw = `type cap.txt`
 
-
 print "#### caps=",stats_raw," \n"
 
-
 caps = stats_raw.scan(/..:..:....../)
-
 print "caps=", caps, "\n\n"
 
 start_str = "00:00:00.000"
