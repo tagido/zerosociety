300d92b81d2f1d5baf15f37fb1c7aad262c3b285
Pedro Silva <tagido@gmail.com>
2017-03-07 17:16:40 +0000

    Code beautifying

--- books/scripts/images_to_ebook.rb
+++ books/scripts/images_to_ebook.rb
@@ -1,9 +1,29 @@
 #
-# images_to_ebook.rb
+#   images_to_ebook.rb
+#   ==================
+#   Converts a set of images to ebooks
+#
+#   Currently supported book formats:
+#   - HTML + images
 #
 #
+#   Copyright (C) 2016 Pedro Mendes da Silva 
+# 
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+# 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+# 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-
+# TODO: automate dependencies and directories (currently hardcoded)
 FFMPEG_PATH="D:\\Program Files (x86)\\FFmpeg for Audacity\\"
 EAC3TO_PATH="D:\\Program Files (x86)\\eac3to331\\"
 BLURAY_PATH="G:."
@@ -79,7 +99,9 @@ def images_to_html_ebook directory, target_filename
 	return html_string
 end
 
-puts "images_to_ebook.rb - Converts images to ebooks"
+puts "images_to_ebook.rb - Converts a set of images to ebooks"
 puts "-------------\n\n"
 
 images_to_html_ebook ".", "index.html"
+
+#TODO: convert HTML to EPUB
